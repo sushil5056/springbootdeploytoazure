462a60a10a61dce1d4727402652c2035cfc1c420
sushil kumar <sushil1thakur@gmail.com>
2022-12-31 14:36:35 +0530

    commit

---
+++ src/main/java/com/sushiltech/azure/SpringbootdeploymentazureApplication.java
@@ -0,0 +1,13 @@
+package com.sushiltech.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeploymentazureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdeploymentazureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushiltech/azure/SpringbootdeploymentazureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushiltech.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdeploymentazureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
